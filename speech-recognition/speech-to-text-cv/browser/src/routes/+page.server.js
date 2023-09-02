bd528d6872c4b205a8d8131fe4ab350ff195b834
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2023-09-02 21:37:29 +0200

    Added audio capture

--- speech-recognition/speech-to-text-cv/browser/src/routes/+page.server.js
+++ speech-recognition/speech-to-text-cv/browser/src/routes/+page.server.js
@@ -1,12 +1,21 @@
-import { SPEECH_KEY, SPEECH_REGION } from "$env/static/private";
-import * as sdk from "microsoft-cognitiveservices-speech-sdk";
-const speechConfig = sdk.SpeechConfig.fromSubscription(
-  SPEECH_KEY,
-  SPEECH_REGION
-);
+import { SPEECH_KEY, SPEECH_REGION } from '$env/static/private';
+import * as sdk from 'microsoft-cognitiveservices-speech-sdk';
+const speechConfig = sdk.SpeechConfig.fromSubscription(SPEECH_KEY, SPEECH_REGION);
+
+const testFunc = () => {
+	return 'lol';
+};
 
 export function load() {
-  return {
-    key: SPEECH_KEY,
-  };
+	return {
+		key: SPEECH_KEY
+	};
 }
+
+export const actions = {
+	recording: async ({ request }) => {
+		const data = await request.formData();
+		const file = data.get('file');
+		console.log(file);
+	}
+};
--- speech-recognition/speech-to-text-cv/browser/src/routes/+page.svelte
+++ speech-recognition/speech-to-text-cv/browser/src/routes/+page.svelte
@@ -1,8 +1,49 @@
 <script>
-    export let data;
+	// export let data;
+	import { onMount } from 'svelte';
+	let media = [];
+	let mediaRecorder = null;
+	let recording = false;
+	// let audioSrc = null;
+	// let fileInput;
+	onMount(async () => {
+		const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+		mediaRecorder = new MediaRecorder(stream);
+		mediaRecorder.ondataavailable = (e) => media.push(e.data);
+		mediaRecorder.onstop = function () {
+			const blob = new Blob(media, { type: 'audio/wav; codecs=opus' });
+			media = [];
+			const audio = document.querySelector('audio');
+			audio.src = window.URL.createObjectURL(blob);
+			// audioSrc = window.URL.createObjectURL(blob);
+			// let file = new File([blob], 'recording.wav', {type: 'audio/wav', lastModified: new Date().getTime()})
+			// let container = new DataTransfer();
+			// container.items.add(file)
+			// const newFileList = container.files;
+			// fileInput.files = newFileList;
+		};
+	});
+	function startRecording() {
+		mediaRecorder.start();
+		recording = true;
+	}
+	function stopRecording() {
+		mediaRecorder.stop();
+		recording = false;
+	}
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Audio based search</h1>
 
-<p>{data.key}</p>
+<section>
+	<audio controls />
+	<button on:click={startRecording} disabled={recording}>Record</button>
+	<button on:click={stopRecording} disabled={!recording}>Stop</button>
+</section>
+<!-- TODO Send file to server -->
+<!-- <form method="POST">
+  <input type="file" name="file" bind:this={fileInput} style="display: none;"/>
+  <button type="submit" disabled={!audioSrc}>Submit</button>
+</form> -->
+
+<p>{media}</p>
