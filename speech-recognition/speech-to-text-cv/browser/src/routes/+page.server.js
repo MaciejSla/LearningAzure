c5aae6945901eb83886a042d93fee0fba556141f
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2023-09-07 22:38:32 +0200

    Added sending file to server

--- speech-recognition/speech-to-text-cv/browser/src/routes/+page.server.js
+++ speech-recognition/speech-to-text-cv/browser/src/routes/+page.server.js
@@ -1,21 +1,50 @@
 import { SPEECH_KEY, SPEECH_REGION } from '$env/static/private';
 import * as sdk from 'microsoft-cognitiveservices-speech-sdk';
 const speechConfig = sdk.SpeechConfig.fromSubscription(SPEECH_KEY, SPEECH_REGION);
+speechConfig.speechRecognitionLanguage = 'pl-PL';
+let recognizedText = '';
 
-const testFunc = () => {
-	return 'lol';
-};
+// ! Possibly rework this
+// function fromFile(file) {
+// 	let audioConfig = sdk.AudioConfig.fromWavFileInput(file);
+// 	let speechRecognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
+
+// 	speechRecognizer.recognizeOnceAsync((result) => {
+// 		switch (result.reason) {
+// 			case sdk.ResultReason.RecognizedSpeech:
+// 				console.log(`RECOGNIZED: Text=${result.text}`);
+// 				break;
+// 			case sdk.ResultReason.NoMatch:
+// 				console.log('NOMATCH: Speech could not be recognized.');
+// 				break;
+// 			case sdk.ResultReason.Canceled:
+// 				const cancellation = sdk.CancellationDetails.fromResult(result);
+// 				console.log(`CANCELED: Reason=${cancellation.reason}`);
+
+// 				if (cancellation.reason == sdk.CancellationReason.Error) {
+// 					console.log(`CANCELED: ErrorCode=${cancellation.ErrorCode}`);
+// 					console.log(`CANCELED: ErrorDetails=${cancellation.errorDetails}`);
+// 					console.log('CANCELED: Did you set the speech resource key and region values?');
+// 				}
+// 				break;
+// 		}
+// 		speechRecognizer.close();
+// 	});
+// }
 
 export function load() {
 	return {
-		key: SPEECH_KEY
+		text: recognizedText
 	};
 }
 
 export const actions = {
-	recording: async ({ request }) => {
+	default: async ({ request }) => {
 		const data = await request.formData();
-		const file = data.get('file');
+		const file = data.get('recording');
 		console.log(file);
+		// fromFile(file);
+		// const data = Object.fromEntries(await request.formData());
+		// console.log(data);
 	}
 };
--- speech-recognition/speech-to-text-cv/browser/src/routes/+page.svelte
+++ speech-recognition/speech-to-text-cv/browser/src/routes/+page.svelte
@@ -1,35 +1,36 @@
 <script>
-	// export let data;
+	export let data;
 	import { onMount } from 'svelte';
 	let media = [];
 	let mediaRecorder = null;
 	let recording = false;
-	// let audioSrc = null;
-	// let fileInput;
+	let audioSrc = null;
+	let fileInput;
 	onMount(async () => {
 		const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
 		mediaRecorder = new MediaRecorder(stream);
 		mediaRecorder.ondataavailable = (e) => media.push(e.data);
 		mediaRecorder.onstop = function () {
-			const blob = new Blob(media, { type: 'audio/wav; codecs=opus' });
+			const blob = new Blob(media, { type: 'audio/wav' });
 			media = [];
 			const audio = document.querySelector('audio');
 			audio.src = window.URL.createObjectURL(blob);
-			// audioSrc = window.URL.createObjectURL(blob);
-			// let file = new File([blob], 'recording.wav', {type: 'audio/wav', lastModified: new Date().getTime()})
-			// let container = new DataTransfer();
-			// container.items.add(file)
-			// const newFileList = container.files;
-			// fileInput.files = newFileList;
+			audioSrc = window.URL.createObjectURL(blob);
+			let file = new File([blob], 'recording.wav', {type: 'audio/wav'})
+			let container = new DataTransfer();
+			container.items.add(file)
+			const newFileList = container.files;
+			fileInput.files = newFileList;
 		};
 	});
-	function startRecording() {
-		mediaRecorder.start();
-		recording = true;
-	}
-	function stopRecording() {
-		mediaRecorder.stop();
-		recording = false;
+	function toggleRecord() {
+		if (recording) {
+			mediaRecorder.stop();
+			recording = false;
+		} else {
+			mediaRecorder.start();
+			recording = true;
+		}
 	}
 </script>
 
@@ -37,13 +38,11 @@
 
 <section>
 	<audio controls />
-	<button on:click={startRecording} disabled={recording}>Record</button>
-	<button on:click={stopRecording} disabled={!recording}>Stop</button>
+	<button on:click={toggleRecord}>Toggle</button>
 </section>
-<!-- TODO Send file to server -->
-<!-- <form method="POST">
-  <input type="file" name="file" bind:this={fileInput} style="display: none;"/>
+<form method="POST" enctype="multipart/form-data">
+  <input type="file" name="recording" bind:this={fileInput} style="display: none"/>
   <button type="submit" disabled={!audioSrc}>Submit</button>
-</form> -->
+</form>
 
-<p>{media}</p>
+<p>{data.text}</p>
