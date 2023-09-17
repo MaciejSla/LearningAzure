8cb5734febf7b4d839dad84833b3283b4b337117
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2023-09-17 21:41:48 +0200

    Added more error catching filters

--- speech-recognition/speech-to-text-cv/browser/src/routes/api/get-completion/+server.js
+++ speech-recognition/speech-to-text-cv/browser/src/routes/api/get-completion/+server.js
@@ -3,6 +3,17 @@ import { OPENAI_API_KEY, OPENAI_API_ENDPOINT } from '$env/static/private';
 import { json } from '@sveltejs/kit';
 
 const regexp = new RegExp(`\{(?:[^{}]|(\{(?:[^{}]|())*\}))*\}`, 'g');
+const fields = [
+	'name',
+	'surname',
+	'age',
+	'mail',
+	'phone',
+	'job',
+	'education',
+	'known_languages',
+	'interests'
+];
 
 export async function POST({ request }) {
 	try {
@@ -11,8 +22,7 @@ export async function POST({ request }) {
 		const messages = [
 			{
 				role: 'system',
-				content:
-					'Assistant is an AI chatbot that turns a search command given by the user into a specific JSON format. The JSON object can contain only these fields: name, surname, age, mail, phone, job, education, known_languages, interests. Assistant should extract matching information from the command and generate a JSON object based on those fields. Fields which can not be filled with information from these commands should be ignored and not included in the final JSON object.  The JSON can contain as little as one field. Assistant should only accept 9 digit phone numbers, not including area codes and dashes or empty spaces between the numbers.'
+				content: `Assistant is an AI chatbot that turns a search command given by the user into a specific JSON format. The JSON object can contain only these fields: ${fields}. Assistant should extract matching information from the command and generate a JSON object based on those fields. Fields which can not be filled with information from these commands should be ignored and not included in the final JSON object.  The JSON can contain as little as one field. Assistant should only accept 9 digit phone numbers, not including area codes and dashes or empty spaces between the numbers.`
 				// "Assistant is an AI chatbot that helps users turn a sentence containing information about a person into a specific JSON format. The JSON object is made out of these fields: name, surname, age, mail, phone, job, education, known_languages, interests. After users input a sentence, assistant will provide a JSON object containing all of the matching information from the sentence. Fields that don't have matching information should be ignored and not included in the final JSON object. Any phone number longer or shorter than 9 digits should be ignored as well. Assistant will automatically format the phone number to be a 9 character string that contains only digits - any area codes should be left out."
 			},
 			{ role: 'user', content: `Parse this command: ${text}` }
@@ -22,7 +32,7 @@ export async function POST({ request }) {
 		const deploymentId = 'gpt-learning';
 		const result = await client.getChatCompletions(deploymentId, messages);
 
-		console.log(result.choices[0].message);
+		// console.log(result.choices[0].message);
 
 		let completion = '';
 
@@ -41,11 +51,18 @@ export async function POST({ request }) {
 					.map((x) => x.charAt(0).toUpperCase() + x.slice(1))
 					.join('-');
 			}
+			if (completion.age) {
+				completion.age =
+					typeof completion.age == 'string' ? parseInt(completion.age) : completion.age;
+			}
 		} catch {
 			return new Response('Incorrect information given - cannot parse input.', { status: 422 });
 		}
 
-		console.log(completion);
+		if (!Object.keys(completion).every((val) => fields.includes(val))) {
+			return new Response('Error parsing input - please try again', { status: 500 });
+		}
+		// console.log(completion);
 
 		return json(completion);
 	} catch (err) {
