ac4d778a2cb0c63ac86699dea66e0ed02a21d6bb
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2023-09-17 16:01:52 +0200

    Modified OpenAI completion formatting

--- speech-recognition/speech-to-text-cv/browser/src/routes/api/get-completion/+server.js
+++ speech-recognition/speech-to-text-cv/browser/src/routes/api/get-completion/+server.js
@@ -2,6 +2,8 @@ import { OpenAIClient, AzureKeyCredential } from '@azure/openai';
 import { OPENAI_API_KEY, OPENAI_API_ENDPOINT } from '$env/static/private';
 import { json } from '@sveltejs/kit';
 
+const regexp = new RegExp(`\{(?:[^{}]|(\{(?:[^{}]|())*\}))*\}`, 'g');
+
 export async function POST({ request }) {
 	try {
 		const { text } = await request.json();
@@ -10,9 +12,10 @@ export async function POST({ request }) {
 			{
 				role: 'system',
 				content:
-					"Assistant is an AI chatbot that helps users turn a sentence containing information about a person into a specific JSON format. The JSON object is made out of these fields: name, surname, age, mail, phone, job, education, known_languages, interests. After users input a sentence, assistant will provide a JSON object containing all of the matching information from the sentence. Fields that don't have matching information should be filled with '-', except known_laguages and interest which should be left as empty arrays instead. Any phone number longer or shorter than 9 digits should be ignored all together and replaced with '-'. Assistant will automatically format the phone number to be a 9 character string that contains only digits - any area codes should be left out."
+					'Assistant is an AI chatbot that turns a search command given by the user into a specific JSON format. The JSON object can contain only these fields: name, surname, age, mail, phone, job, education, known_languages, interests. Assistant should extract matching information from the command and generate a JSON object based on those fields. Fields which can not be filled with information from these commands should be ignored and not included in the final JSON object.  The JSON can contain as little as one field. Assistant should only accept 9 digit phone numbers, not including area codes and dashes or empty spaces between the numbers.'
+				// "Assistant is an AI chatbot that helps users turn a sentence containing information about a person into a specific JSON format. The JSON object is made out of these fields: name, surname, age, mail, phone, job, education, known_languages, interests. After users input a sentence, assistant will provide a JSON object containing all of the matching information from the sentence. Fields that don't have matching information should be ignored and not included in the final JSON object. Any phone number longer or shorter than 9 digits should be ignored as well. Assistant will automatically format the phone number to be a 9 character string that contains only digits - any area codes should be left out."
 			},
-			{ role: 'user', content: text }
+			{ role: 'user', content: `Parse this command: ${text}` }
 		];
 
 		const client = new OpenAIClient(OPENAI_API_ENDPOINT, new AzureKeyCredential(OPENAI_API_KEY));
@@ -24,19 +27,12 @@ export async function POST({ request }) {
 		let completion = '';
 
 		try {
-			completion = JSON.parse(
-				'{' +
-					result.choices[0].message.content
-						.split('{')[1]
-						.replace('\n', '')
-						.replace("'", '"')
-						.split('}')[0] +
-					'}'
-			);
-			if (completion.name != '-') {
+			completion = JSON.parse(regexp.exec(result.choices[0].message.content)[0]);
+			regexp.lastIndex = 0;
+			if (completion.name) {
 				completion.name = completion.name.charAt(0).toUpperCase() + completion.name.slice(1);
 			}
-			if (completion.surname != '-') {
+			if (completion.surname) {
 				completion.surname = completion.surname
 					.split(' ')
 					.map((x) => x.charAt(0).toUpperCase() + x.slice(1))
