7c2377f1b546e9ee6063d8afd2bd99310f87698b
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2023-09-19 20:54:44 +0200

    Reformatted and cleaned up

--- speech-recognition/speech-to-text-cv/browser/src/routes/api/db/getUser/+server.js
+++ speech-recognition/speech-to-text-cv/browser/src/routes/api/db/getUser/+server.js
@@ -29,25 +29,7 @@ export async function POST({ request }) {
 		delete filter.interests;
 	}
 
-	let users;
-
 	async function main() {
-		// ? Create user schema
-		// const user = await prisma.employees.create({
-		// 	data: {
-		// 		name: 'Stefan',
-		// 		surname: 'Woźniak',
-		// 		age: 73,
-		// 		mail: 'steve.wozniak@gmail.com',
-		// 		phone: '123456789',
-		// 		job: 'wynalazca',
-		// 		education: 'UC Berkely College of Engineering',
-		// 		known_languages: {
-		// 			create: [{ name: 'angielski' }, { name: 'niemiecki' }]
-		// 		}
-		// 	}
-		// });
-		// console.log(user);
 		const raw = await prisma.employees.findMany({
 			include: {
 				known_languages: true,
@@ -55,22 +37,13 @@ export async function POST({ request }) {
 			},
 			where: filter
 		});
-		users = raw.map((x) => ({
+		const users = raw.map((x) => ({
 			...x,
 			known_languages: Object.values(x.known_languages).map((y) => y.name),
 			interests: Object.values(x.interests).map((y) => y.name)
 		}));
+		return json(users);
 	}
 
-	await main()
-		.then(async () => {
-			await prisma.$disconnect();
-		})
-		.catch(async (e) => {
-			console.error(e);
-			await prisma.$disconnect();
-			process.exit(1);
-		});
-
-	return json(users);
+	return await main();
 }
