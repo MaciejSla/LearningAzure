a6fc35e33736254c3a8642e957ef9ba757f36f80
Maciej Sładkiewicz <m.sladkiewicz.633@studms.ug.edu.pl>
2023-09-21 18:24:58 +0200

    Changed database import format to allow exclusion of sample records

--- speech-recognition/speech-to-text-cv/db-import/create.sql
+++ speech-recognition/speech-to-text-cv/db-import/create.sql
@@ -41,15 +41,3 @@ ALTER TABLE `languages` ADD CONSTRAINT `languages_userId_fkey` FOREIGN KEY (`use
 
 -- AddForeignKey
 ALTER TABLE `interests` ADD CONSTRAINT `interests_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `employees`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
-
-INSERT INTO employees VALUES 
-  (1, 'Stefan', 'Woźniak', 73, 'steve.wozniak@gmail.com', '123456789', 'wynalazca', 'UC Berkely College of Engineering'),
-  (2, 'Mariusz', 'Pudzianowski', 66, 'm.pudzian@poczta.onet.pl', '987654321', 'strongman', 'Społeczna Wyższa Szkoła Przedsiębiorczości i Zarządzania w Łodzi');
-
-INSERT INTO languages VALUES 
-  (1, 'angielski', 1),
-  (2, 'niemiecki', 1),
-  (3, 'polski', 2);
-
-INSERT INTO interests VALUES (1, 'programowanie', 1);
---
+++ speech-recognition/speech-to-text-cv/db-import/insert.sql
@@ -0,0 +1,12 @@
+USE testdb;
+
+INSERT INTO employees VALUES 
+  (1, 'Stefan', 'Woźniak', 73, 'steve.wozniak@gmail.com', '123456789', 'wynalazca', 'UC Berkely College of Engineering'),
+  (2, 'Mariusz', 'Pudzianowski', 66, 'm.pudzian@poczta.onet.pl', '987654321', 'strongman', 'Społeczna Wyższa Szkoła Przedsiębiorczości i Zarządzania w Łodzi');
+
+INSERT INTO languages VALUES 
+  (1, 'angielski', 1),
+  (2, 'niemiecki', 1),
+  (3, 'polski', 2);
+
+INSERT INTO interests VALUES (1, 'programowanie', 1);
